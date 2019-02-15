08b89cba938c8fc5824a1ebe3e4adf88d52b9135
bogdan_scher <bsc@piogroup.com>
2019-02-15 12:27:55 +0200

    rebasing

--- src/version/sagas/index.js
+++ src/version/sagas/index.js
@@ -1,12 +1,6 @@
 import feedsListSagas from "./feedsListSagas";
 import feedsRssSagas from "./feedsRssSagas";
 
-/**
- * This componenr returns array of version sagas
- *
- * @author Bogdan Shcherban <bsc@piogroup.net>
- * @return {array}
- */
 export default [
     feedsListSagas,
     feedsRssSagas,
